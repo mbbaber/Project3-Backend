e31459ecbb04e48d560957275e4fffb9e9d91ebf
kucharskap <kucharskap@gmail.com>
2018-05-12 22:56:56 +0200

    adding groups (admin rights) added

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const authRoutes = express.Router();
 const User = require("../models/User");
 const mongoose = require("mongoose");
@@ -10,8 +10,7 @@ const bcryptSalt = 10;
 // AUTH - edit login feature
 
 authRoutes.post("/login", (req, res, next) => {
-  const myFunction =
-  passport.authenticate("local", (err, theUser) => {
+  const myFunction = passport.authenticate("local", (err, theUser) => {
     if (err) {
       next(err);
       return;
@@ -24,24 +23,24 @@ authRoutes.post("/login", (req, res, next) => {
     }
     req.login(theUser, () => {
       theUser.password = undefined; // clear the password before sending (security risk)
-      res.json({userInfo: theUser });
+      res.json({ userInfo: theUser });
     });
-});
-myFunction(req, res, next);
+  });
+  myFunction(req, res, next);
 });
 
 // AUTH signup feature
 
 authRoutes.post("/signup", (req, res, next) => {
-  const { username, password, email}  = req.body;
-  
-  console.log(req.body)
+  const { username, password, email } = req.body;
+
+  console.log(req.body);
 
   const rol = req.body.role;
   if (username === "" || password === "") {
     // 2) instead of rendering, we will do this:
     //res.render("auth/signup", { message: "Indicate username and password" });
-    const err = new Error("Username or password invalid"); 
+    const err = new Error("Username or password invalid");
     err.status = 400; //add generic client error (when the user f's up)
     next();
     return;
@@ -51,7 +50,7 @@ authRoutes.post("/signup", (req, res, next) => {
     if (user !== null) {
       // 3)
       //res.render("auth/signup", { message: "The username already exists" });
-      const err = new Error("The username already exists"); 
+      const err = new Error("The username already exists");
       err.status = 400;
       next(err);
       return;
@@ -62,17 +61,18 @@ authRoutes.post("/signup", (req, res, next) => {
     const newUser = new User({
       username,
       email,
-      password: hashPass,
+      password: hashPass
     });
 
-    newUser.save((err) => {
+    newUser.save(err => {
       if (err) {
         next(err);
       } else {
-        req.login(newUser, ()=> { //use this if you don't want an email confirmation (just logs in user automatically after signup and saves a step)
-        newUser.password = undefined;
-        res.json({ userInfo: newUser }); // will connect userInfo to angular side soon
-        }); 
+        req.login(newUser, () => {
+          //use this if you don't want an email confirmation (just logs in user automatically after signup and saves a step)
+          newUser.password = undefined;
+          res.json({ userInfo: newUser }); // will connect userInfo to angular side soon
+        });
       }
     });
   });
@@ -85,30 +85,29 @@ authRoutes.get("/logout", (req, res) => {
 
 authRoutes.get("/checklogin", (req, res, next) => {
   if (req.user) {
-    req.user.password = undefined
+    req.user.password = undefined;
   }
-  res.json({userInfo: req.user });
+  res.json({ userInfo: req.user });
 });
 
-
-authRoutes.get('/user/:userId', (req, res, next)=>{
+authRoutes.get("/user/:userId", (req, res, next) => {
   // if (!mongoose.Types.ObjectId.isValid(req.params.groupId)) {
   //   next(); // show 404 if bad ObjectId format
   //   return;
   // }
-  
+
   User.findById(req.params.userId)
-  //.populate("subjects")
-    .then((user) => {
+    //.populate("subjects")
+    .then(user => {
       if (!user) {
         next(); // show 404 if no group was found
         return;
       }
       res.json(user);
     })
-    .catch((err) => {
+    .catch(err => {
       next(err);
     });
-})
+});
 
 module.exports = authRoutes;
--- routes/group.js
+++ routes/group.js
@@ -1,45 +1,80 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
 const Group = require("../models/Group");
 const Subject = require("../models/Subject");
 const Stat = require("../models/Stat");
-const User = require('../models/User');
+const User = require("../models/User");
 
-router.get('/groups', (req, res, next) => {
-    Group.find()
-      .then((groups) => {
-        res.json(groups);
-      })
-      .catch((err) => {
-        next(err);
-      });
-  });
-  
-  router.get('/user-groups/:userId', (req, res, next)=>{
-    User.findById(req.params.userId)
-    .populate('groups')
-    .then((result)=>{
-      res.json(result.groups);
+router.get("/groups", (req, res, next) => {
+  Group.find()
+    .then(groups => {
+      res.json(groups);
     })
-    .catch((err)=>{
+    .catch(err => {
       next(err);
-    })
-  })
-   
-  router.put('/groups-of-the-user/:userId/gr/:groupId', (req, res, next)=>{
-    User.findByIdAndUpdate(
-      req.params.userId,
-      {$pull: {groups: req.params.groupId}})
-      .populate('groups')
-    .then((result)=>{
-      console.log(result.groups)
+    });
+});
+
+router.get("/user-groups/:userId", (req, res, next) => {
+  User.findById(req.params.userId)
+    .populate("groups")
+    .then(result => {
       res.json(result.groups);
     })
-    .catch((err)=>{
-      next('error deleting groups',err);
-    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.put("/groups-of-the-user/:userId/gr/:groupId", (req, res, next) => {
+  User.findByIdAndUpdate(req.params.userId, {
+    $pull: { groups: req.params.groupId }
   })
+    .populate("groups")
+    .then(result => {
+      console.log(result.groups);
+      res.json(result.groups);
+    })
+    .catch(err => {
+      next("error deleting groups", err);
+    });
+});
+
+router.post("/new-group", (req, res, next) => {
+  const newGroup = new Group({
+    name: req.body.name,
+    admin: req.user
+  });
+  let newGroupId = "";
+  newGroup.save(err => {
+    if (err) {
+      next(err);
+    } else {
+      res.json(newGroup);
+      newGroupId = newGroup._id;
+      console.log(newGroupId);
+      console.log(req.user);
+      User.findByIdAndUpdate(req.user, { $push: { groups: newGroupId } })
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => {
+          next(err);
+        });
+    }
+  });
+});
+
+router.get("/new-group/:groupId", (req, res, next) => {
+  Group.findById(req.params.groupId)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
